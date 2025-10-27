1381f99ff81bd33fb31041579b6f21373c577ca4
nexphernandez <nicolaseh3b1112@gmail.com>
2025-10-27 15:28:54 +0000

    Se empieza a realizar el ejercicio

---
+++ generador_de_nombres/android/app/src/main/kotlin/com/example/generador_de_nombres/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.generador_de_nombres
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ generador_de_nombres/lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      // ← ?? Lo entenderemos más adelante
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepOrange,
+          ), // ← Los colores del tema son deepOrange
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context
+        .watch<MyAppState>(); // ← ?? Lo entenderemos más adelante
+    final pair = appState.current;
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('A random idea:'),
+          BigData(pair: pair),
+          ElevatedButton(
+            onPressed: () {
+              appState.getNext();
+            },
+            child: Text("Siguiente"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BigData extends StatelessWidget {
+  const BigData({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(pair.asLowerCase),
+      ),
+    );
+  }
+}
